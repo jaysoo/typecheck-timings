0b8b1222b74da870c89b11235e6a004733e843c3
Jack Hsu <jack.hsu@gmail.com>
2025-01-02 09:59:30 -0500

    add memory usage info

--- README.md
+++ README.md
@@ -2,26 +2,28 @@ This repo aims to provide typecheck comparisons between the old (integrated) TS 
 
 The new setup should be faster on average, but in the cold cache case, it will be slower by definition. Several scenarios are tested:
 
-Run the timings:
+Run the timings (if you are on MacOS then you will need `gtime` installed -- use `brew install gnu-time`):
 
 ```shell
 node bench.mjs
 ```
+
+NOTE: This script does not work on Windows currently due to missing `time` command to capture memory usage.
 
 These are the results on my machine:
 
 ```shell
 INTEGRATED SETUP (OLD)
 ----------------------
-Timing typecheck... 63.19s
+Timing typecheck... 61.80s (max memory: 2.70 GB)
 
 TS SOLUTION SETUP (NEW)
 -----------------------
-Timing typecheck (cold)... 120.06s
-Timing typecheck (hot)... 0.35s
-Timing typecheck (warm - 1 pkg updated)... 9.69s
-Timing typecheck (warm - 5 pkg updated)... 45.06s
-Timing typecheck (warm - 1 nested leaf pkg updated)... 14.19s
-Timing typecheck (warm - 2 nested leaf pkg updated)... 28.03s
-Timing typecheck (warm - 1 nested root pkg updated)... 1.29s
+Timing typecheck (cold)... 116.74s (max memory: 1.57 GB)
+Timing typecheck (hot)... 0.57s (max memory: 101.60 MB)
+Timing typecheck (warm - 1 pkg updated)... 9.81s (max memory: 685.73 MB)
+Timing typecheck (warm - 5 pkg updated)... 45.63s (max memory: 935.87 MB)
+Timing typecheck (warm - 1 nested leaf pkg updated)... 14.31s (max memory: 796.62 MB)
+Timing typecheck (warm - 2 nested leaf pkg updated)... 27.36s (max memory: 944.88 MB)
+Timing typecheck (warm - 1 nested root pkg updated)... 1.38s (max memory: 243.01 MB)
 ```
--- bench.mjs
+++ bench.mjs
@@ -1,98 +1,6 @@
 import * as cp from 'node:child_process';
 import * as fs from 'node:fs';
 
-let start, end;
-const originals = new Map();
-
-console.log('Timing how long typechecks take for old vs new TS setup. This will take a few minutes.\n');
-
-console.log('INTEGRATED SETUP (OLD)');
-console.log('----------------------');
-
-process.stdout.write('Timing typecheck... ');
-cleanOutput();
-start = performance.now();
-cp.execSync('npx nx typecheck demo --skip-nx-cache', {
-  cwd: 'ts-integrated',
-});
-end = performance.now();
-process.stdout.write(`${toSeconds(end - start)}\n`);
-
-console.log('\nTS SOLUTION SETUP (NEW)');
-console.log('-----------------------');
-
-cleanOutput();
-
-process.stdout.write('Timing typecheck (cold)... ')
-start = performance.now();
-cp.execSync('npx nx typecheck demo --skip-nx-cache', {
-  cwd: 'ts-solution',
-});
-end = performance.now();
-process.stdout.write(`${toSeconds(end - start)}\n`);
-
-process.stdout.write('Timing typecheck (hot)... ');
-start = performance.now();
-cp.execSync('npx nx typecheck demo', {
-  cwd: 'ts-solution',
-});
-end = performance.now();
-process.stdout.write(`${toSeconds(end - start)}\n`);
-
-process.stdout.write('Timing typecheck (warm - 1 pkg updated)... ');
-updatePkg(1);
-start = performance.now();
-cp.execSync('npx nx typecheck demo', {
-  cwd: 'ts-solution',
-});
-end = performance.now();
-process.stdout.write(`${toSeconds(end - start)}\n`);
-
-process.stdout.write('Timing typecheck (warm - 5 pkg updated)... ');
-updatePkg(2);
-updatePkg(3);
-updatePkg(4);
-updatePkg(5);
-updatePkg(6);
-start = performance.now();
-cp.execSync('npx nx typecheck demo', {
-  cwd: 'ts-solution',
-});
-end = performance.now();
-process.stdout.write(`${toSeconds(end - start)}\n`);
-
-process.stdout.write('Timing typecheck (warm - 1 nested leaf pkg updated)... ');
-updateNestedLeafPkg(1);
-start = performance.now();
-cp.execSync('npx nx typecheck demo', {
-  cwd: 'ts-solution',
-});
-end = performance.now();
-process.stdout.write(`${toSeconds(end - start)}\n`);
-
-process.stdout.write('Timing typecheck (warm - 2 nested leaf pkg updated)... ');
-updateNestedLeafPkg(1);
-updateNestedLeafPkg(2);
-start = performance.now();
-cp.execSync('npx nx typecheck demo', {
-  cwd: 'ts-solution',
-});
-end = performance.now();
-process.stdout.write(`${toSeconds(end - start)}\n`);
-
-process.stdout.write('Timing typecheck (warm - 1 nested root pkg updated)... ');
-updateNestedRootPkg();
-start = performance.now();
-cp.execSync('npx nx typecheck demo', {
-  cwd: 'ts-solution',
-});
-end = performance.now();
-process.stdout.write(`${toSeconds(end - start)}\n`);
-
-resetChanges();
-
-/* -------------------------------------------------------------------------- */
-
 function cleanOutput() {
   cp.execSync(`find packages -name out-tsc | xargs rm -rf`, {
     cwd: 'ts-solution',
@@ -103,35 +11,160 @@ function toSeconds(ms) {
   return `${(ms / 1000).toFixed(2)}s`;
 }
 
-
 function updatePkg(i) {
   const file = `./ts-solution/packages/lib-${i}/src/index.tsx`;
   const content = fs.readFileSync(file).toString();
-  originals.set(file, content);
   fs.writeFileSync(file, `${content};\nexport const foo${randInt()} = 'foo'`);
 }
 
 function updateNestedLeafPkg(i) {
   const file = `./ts-solution/packages/nested-1-${i}/src/index.tsx`;
   const content = fs.readFileSync(file).toString();
-  originals.set(file, content);
   fs.writeFileSync(file, `${content};\nexport const foo${randInt()} = 'foo'`);
 }
 
 function updateNestedRootPkg() {
   const file = `./ts-solution/packages/nested-1/src/index.tsx`;
   const content = fs.readFileSync(file).toString();
-  originals.set(file, content);
   fs.writeFileSync(file, `${content};\nexport const foo${randInt()} = 'foo'`);
 }
 
 function resetChanges() {
-  originals.forEach(([file, content]) => {
-    fs.writeFileSync(file, content);
-  });
-  originals.clear();
+  cp.execSync('git checkout ts-solution');
 }
 
 function randInt() {
   return Math.ceil(Math.random() * 1000);
 }
+
+function formatMemory(kb) {
+  const m = parseInt(kb);
+  if (m >= 1_000_000) {
+    return `${(m / 1_000_000).toFixed(2)} GB`;
+  } else if (m >= 1000)  {
+    return `${(m / 1_000).toFixed(2)} MB`;
+  } else {
+    return `${m} KB`;
+  }
+}
+
+async function exec(cmd, opts) {
+  return new Promise((resolve) => {
+    let data = '';
+    const p = cp.exec(cmd, opts);
+    p.stdout.on('data', (buf) => {
+      data += buf.toString();
+    });
+    p.stderr.on('data', (buf) => {
+      data += buf.toString();
+    });
+    p.on('close', () => {
+      resolve(data);
+    });
+  });
+}
+
+process.on('SIGINT', () => {
+  resetChanges()
+  process.exit();
+});
+process.on('exit', () => resetChanges());
+
+let start, end, output, mem;
+
+const timeCmd = process.platform === 'darwin' ? 'gtime' : 'time';
+const memRegexp = /Maximum resident set size \(kbytes\): (?<mem>\d+)/;
+
+console.log('Timing how long typechecks take for old vs new TS setup. This will take a few minutes.\n');
+
+console.log('INTEGRATED SETUP (OLD)');
+console.log('----------------------');
+
+process.stdout.write('Timing typecheck... ');
+cleanOutput();
+start = performance.now();
+output = await exec(`${timeCmd} -v npx nx typecheck demo --skip-nx-cache`, {
+  cwd: 'ts-integrated',
+});
+end = performance.now();
+mem = output.match(memRegexp).groups.mem;
+process.stdout.write(`${toSeconds(end - start)} (max memory: ${formatMemory(mem)})\n`);
+
+console.log('\nTS SOLUTION SETUP (NEW)');
+console.log('-----------------------');
+
+cleanOutput();
+
+process.stdout.write('Timing typecheck (cold)... ')
+start = performance.now();
+output = await exec(`${timeCmd} -v npx nx typecheck demo --skip-nx-cache`, {
+  cwd: 'ts-solution',
+});
+end = performance.now();
+mem = output.match(memRegexp).groups.mem;
+process.stdout.write(`${toSeconds(end - start)} (max memory: ${formatMemory(mem)})\n`);
+
+process.stdout.write('Timing typecheck (hot)... ');
+start = performance.now();
+output = await exec(`${timeCmd} -v npx nx typecheck demo`, {
+  cwd: 'ts-solution',
+});
+end = performance.now();
+mem = output.match(memRegexp).groups.mem;
+process.stdout.write(`${toSeconds(end - start)} (max memory: ${formatMemory(mem)})\n`);
+
+process.stdout.write('Timing typecheck (warm - 1 pkg updated)... ');
+updatePkg(1);
+start = performance.now();
+output = await exec(`${timeCmd} -v npx nx typecheck demo`, {
+  cwd: 'ts-solution',
+});
+end = performance.now();
+mem = output.match(memRegexp).groups.mem;
+process.stdout.write(`${toSeconds(end - start)} (max memory: ${formatMemory(mem)})\n`);
+
+process.stdout.write('Timing typecheck (warm - 5 pkg updated)... ');
+updatePkg(2);
+updatePkg(3);
+updatePkg(4);
+updatePkg(5);
+updatePkg(6);
+start = performance.now();
+output = await exec(`${timeCmd} -v npx nx typecheck demo`, {
+  cwd: 'ts-solution',
+});
+end = performance.now();
+mem = output.match(memRegexp).groups.mem;
+process.stdout.write(`${toSeconds(end - start)} (max memory: ${formatMemory(mem)})\n`);
+
+process.stdout.write('Timing typecheck (warm - 1 nested leaf pkg updated)... ');
+updateNestedLeafPkg(1);
+start = performance.now();
+output = await exec(`${timeCmd} -v npx nx typecheck demo`, {
+  cwd: 'ts-solution',
+});
+end = performance.now();
+mem = output.match(memRegexp).groups.mem;
+process.stdout.write(`${toSeconds(end - start)} (max memory: ${formatMemory(mem)})\n`);
+
+process.stdout.write('Timing typecheck (warm - 2 nested leaf pkg updated)... ');
+updateNestedLeafPkg(1);
+updateNestedLeafPkg(2);
+start = performance.now();
+output = await exec(`${timeCmd} -v npx nx typecheck demo`, {
+  cwd: 'ts-solution',
+});
+end = performance.now();
+mem = output.match(memRegexp).groups.mem;
+process.stdout.write(`${toSeconds(end - start)} (max memory: ${formatMemory(mem)})\n`);
+
+process.stdout.write('Timing typecheck (warm - 1 nested root pkg updated)... ');
+updateNestedRootPkg();
+start = performance.now();
+output = await exec(`${timeCmd} -v npx nx typecheck demo`, {
+  cwd: 'ts-solution',
+});
+end = performance.now();
+mem = output.match(memRegexp).groups.mem;
+process.stdout.write(`${toSeconds(end - start)} (max memory: ${formatMemory(mem)})\n`);
+
